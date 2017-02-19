e924057204932f1023decd007d8865ba24f89ac5
Aleksey <novikov_9292@mail.ru>
2017-02-19 22:22:20 +0600

    Пакет "Интерфейс"

--- plugins/diductio-subscriber/view/my.php
+++ plugins/diductio-subscriber/view/my.php
@@ -1,7 +1,7 @@
 <?php if(is_user_logged_in()): ?>
 <div class="stat-col">
 	<a href="/my-subscriptions">
-		<span class="label label-success <?=$data->class;?>">Моё</span>
+		<span class="label label-success <?=$data->class;?>">Подписки</span>
 		<span class="label label-success"><?=$data->number_of_posts;?></span>
 	</a>
 </div>
