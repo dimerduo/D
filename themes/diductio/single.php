7d91c861ad67f9113e1b8cce1dc4e008785df433
Mrau Hu <mrauhu@yandex.ru>
2017-03-07 00:38:31 +0300

    Remove link from header of total progress.

--- themes/diductio/single.php
+++ themes/diductio/single.php
@@ -147,10 +147,8 @@
 
 	        <div class="col-sm-12 col-md-12">
 		        <div>
-			        <a name="total">
-				        <!-- <img> for avatar -->
-				        <span>Общий прогресс</span>
-			        </a>
+			        <!-- <img> for avatar -->
+			        <span>Общий прогресс</span>
 		        </div>
 		        <div class="progress">
 			        <div class="progress-bar " role="progressbar" aria-valuenow="<?= $total_progress; ?>" aria-valuemin="0" aria-valuemax="100" style="width:<?=$posts_users[$i]['progress'];?>%;">
