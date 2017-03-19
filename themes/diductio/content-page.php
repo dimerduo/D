3c8a78dd9a3c8edb5c4823fa403acd519fef939f
Aleksey <novikov_9292@mail.ru>
2017-03-19 13:54:57 +0600

    Пакет «Мелкие правки и ошибки»

--- themes/diductio/content-page.php
+++ themes/diductio/content-page.php
@@ -15,18 +15,26 @@
 	?>
 
 	<header class="entry-header">
-	 	<?php $id = get_the_ID(); 
-
+	 	<?php $id = get_the_ID();
 	 	if($id != '68' && !is_page('progress') ): ?>
-			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		    <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 		<?php elseif(is_page('progress')):
-			$current_user = wp_get_current_user(); ?>
-			<div  class="avatar inline ">
-				<?=get_avatar( $current_user->user_email, 96 );?></div>
-		        <h1 style="margin-left: 20px;" class="inline entry-title"><?php print_r($current_user->data->display_name); ?></h1>
-		    <div style="margin-bottom: 20px;">
-				<div class="about"><?=get_user_meta($current_user->ID,'description')[0];?></div>
-			</div>
+			$current_user = wp_get_current_user();
+            $category_statistic = $GLOBALS['st']->get_categories_stat_by_post($current_user->ID);
+            ?>
+            <div class="personal-area">
+                <div  class="avatar inline">
+                    <?=get_avatar( $current_user->user_email, 96 );?>
+                </div>
+                <div style="margin-left: 20px;" class="inline">
+                    <h1 class="inline entry-title"><?php print_r($current_user->data->display_name); ?></h1>
+                    <div class="about"><?=get_user_meta($current_user->ID,'description')[0];?></div>
+                    <div class="user-categories">
+                        <?php view('user-category-static', compact('category_statistic', 'current_user')); ?>
+                    </div>
+                </div>
+
+            </div>
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
