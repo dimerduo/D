bfdf538a1d7e8cd262bc355409c7d563cbab0902
Aleksey <novikov_9292@mail.ru>
2017-09-24 20:26:53 +0600

    Убран заголовок блока комментария

--- themes/diductio/comments.php
+++ themes/diductio/comments.php
@@ -23,13 +23,6 @@ if ( post_password_required() ) {
 <div id="comments" class="comments-area">
 
 	<?php if ( have_comments() ) : ?>
-		<h2 class="comments-title">
-			<?php
-				printf( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'diductio' ),
-					number_format_i18n( get_comments_number() ), get_the_title() );
-			?>
-		</h2>
-
 		<?php twentyfifteen_comment_nav(); ?>
 
 		<ol class="comment-list">
